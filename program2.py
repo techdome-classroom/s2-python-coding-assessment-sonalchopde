ca7e1d6d0711a4569d3c0a0316cc3f02918a200a
Sonal Chopde <sonal242003@gmail.com>
2024-09-27 13:59:39 +0530

    python program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,12 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        for i in range(len(s) - 1):
+            if roman_numerals[s[i]] < roman_numerals[s[i + 1]]:
+                total -= roman_numerals[s[i]]
+            else:
+                total += roman_numerals[s[i]]
+        return total + roman_numerals[s[-1]]
 
 
